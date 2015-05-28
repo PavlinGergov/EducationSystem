95811398125c93e5827d952b830949436092abd5
Margarita Vasileva <m.h.vasileva@gmail.com>
2015-05-28 13:45:54 +0300

    fix login

--- app/scripts/auth/auth.service.js
+++ app/scripts/auth/auth.service.js
@@ -10,26 +10,27 @@
       register: register,
       login: login,
       activate: activate,
+      logout: logout,
       splitName: splitName
     };
     
     return service;
 
+    function logout() {
+      //send delete request
+    }
     function register(user, url) {
-      console.log();
       //send url
       return $http.post(DATA_URL + 'register/', user)
         .then(function(response) {
-          //success
-          console.log(response);
+          return response;
         });
     };
 
     function login(user) {
       return $http.post(DATA_URL + 'login/', user)
         .then(function(response) {
-          console.log(response);
-          //success
+          return response.data;
         });
     }
 
