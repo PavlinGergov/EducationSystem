921e6350ec7b71f000b7888e2f7ee7ae0950c976
Margarita Vasileva <m.h.vasileva@gmail.com>
2015-06-01 20:00:15 +0300

    improve profile page

--- app/scripts/auth/auth.profile.js
+++ app/scripts/auth/auth.profile.js
@@ -7,8 +7,47 @@
   
   function profileCtrl(user) {
     var vm = this;
-    vm.user = user;
-
-    console.log(vm.user);
-  };
+    vm.user = {
+      first_name: 'Margarita',
+      last_name: 'Vasileva',
+      email: 'm.vasileva@example.com',
+      avatar: 'https://s-media-cache-ak0.pinimg.com/736x/10/61/61/1061614ee7f3a3e64be576c2cc04d13e.jpg',
+      social_links: {
+        github: 'https://github.com/Shosh',
+        twitter: 'https://twitter.com/sho_clk',
+        linkedin: '-'        
+      },
+      courses: [
+        {
+          name: 'Programming 101 v2',
+          status: 'done',
+          notes: 'notes here'
+        },
+        {
+          name: 'Ruby on Rails',
+          status: 'taking',
+          notes: 'notes2 here'
+        },
+        {
+          name: 'C',
+          status: 'dropped',
+          notes: 'notes3 here'
+        }
+      ],
+      challenges: [
+        {
+          hackathon: 'HackFMI5',
+          team_name: 'Example name',
+          idea_description: 'Example idea',
+          place: 1
+        },
+        {
+          hackathon: 'HackBulgaria Hackathon',
+          team_name: 'Test name',
+          idea_description: 'Test idea',
+          place: 'N/A'
+        }
+      ]
+    };
+  }
 })();
