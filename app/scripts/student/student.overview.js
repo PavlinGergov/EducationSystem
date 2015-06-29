e5e4ee24c4df497c2a5ea2641fe5a826231958d7
Margarita Vasileva <m.h.vasileva@gmail.com>
2015-06-29 17:24:32 +0300

    show table data

--- app/scripts/student/student.overview.js
+++ app/scripts/student/student.overview.js
@@ -5,13 +5,23 @@
     .module('educationSystemApp.student')
     .controller('overviewCtrl', overviewCtrl);
   
-  function overviewCtrl() {
+  function overviewCtrl(tableData, user, studentService, $stateParams) {
     var vm = this;
-    
+
     activate();
+    console.log(user);
+    console.log($stateParams);
     
     function activate() {
-      
+      var courseId = $stateParams.courseId;
+      var studentId = user.student.id;
+      var emptyTable = tableData.data;
+      vm.weekdays = tableData.weekdays;
+
+      studentService.getStudentCheckins(studentId, courseId)
+        .then(function(checkins) {
+          vm.presence = studentService.getPresenceTable(emptyTable, checkins);
+        });
     };
   };
 })();
