153b93a170b7edcc295fce5f550eae9505fddd13
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2015-07-01 16:05:29 +0300

    Course statistics helper functions, htlm and JS

--- app/scripts/teacher/teacher.course.statistics.js
+++ app/scripts/teacher/teacher.course.statistics.js
@@ -1,17 +1,40 @@
 (function() {
   'use strict';
-  
+
   angular
     .module('educationSystemApp.teacher')
     .controller('courseStatisticsCtrl', courseStatisticsCtrl);
-  
-  function courseStatisticsCtrl() {
+
+  function courseStatisticsCtrl($stateParams, studentService, teacherService) {
     var vm = this;
-    
+
     activate();
-    
+
     function activate() {
-      
-    };
-  };
+      vm.courseId = $stateParams.courseId;
+
+      teacherService.getAvgPresenceForCourseWithDropped(vm.courseId)
+        .then(function(avgPresence) {
+          vm.avgPresenceWithDropped = avgPresence;
+        });
+
+      teacherService.getAvgPresenceForCourseWithoutDropped(vm.courseId)
+        .then(function(avgPresence) {
+          vm.avgPresenceWithoutDropped = avgPresence;
+        });
+
+      teacherService.getCAsForCourse(vm.courseId)
+        .then(function(cas) {
+          vm.studentsStarted = cas.length;
+          vm.studentsDropped = 0;
+          cas.forEach(function(ca) {
+            if (!ca.is_attending) { vm.studentsDropped += 1; }
+          });
+          vm.dropRate = vm.studentsDropped / vm.studentsStarted;
+        });
+
+
+    }
+
+  }
 })();
--- app/scripts/teacher/teacher.service.js
+++ app/scripts/teacher/teacher.service.js
@@ -8,7 +8,10 @@
   function teacherService($http, EDUCATION_URL) {
     var service = {
       getTeachedCourses: getTeachedCourses,
-      getCAsForCourse: getCAsForCourse
+      getCAsForCourse: getCAsForCourse,
+      getAvgPresenceForCourseWithDropped: getAvgPresenceForCourseWithDropped,
+      getAvgPresenceForCourseWithoutDropped: getAvgPresenceForCourseWithoutDropped
+
     };
 
     return service;
@@ -17,7 +20,7 @@
       console.log(user);
       return user.teacher.teached_courses.reverse();
     }
-      
+
     function getCAsForCourse(courseId) {
       var options = { headers: { 'Authorization': 'Token ' + localStorage.getItem('token') }};
       return $http.get(EDUCATION_URL + 'get-cas-for-course/?course_id=' + courseId, options)
@@ -25,5 +28,33 @@
           return response.data;
         });
     }
+
+    function getAvgPresenceForCourseWithDropped(courseId) {
+      return getCAsForCourse(courseId)
+        .then(function(cas) {
+          var totalPresence = 0;
+          var students = cas.length;
+          cas.forEach(function(ca) {
+            totalPresence += Number(ca.student_presence);
+          });
+          return totalPresence / students;
+        });
+    }
+
+    function getAvgPresenceForCourseWithoutDropped(courseId) {
+      return getCAsForCourse(courseId)
+        .then(function(cas) {
+          var totalPresence = 0;
+          var students = 0;
+          cas.forEach(function(ca) {
+            if (ca.is_attending) {
+              totalPresence += Number(ca.student_presence);
+              students += 1;
+            }
+          });
+          return totalPresence / students;
+        });
+    }
+
   }
 })();
