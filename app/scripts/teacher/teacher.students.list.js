bff52581ff8ee112835cf61ba39779c5553a65e8
Margarita Vasileva <m.h.vasileva@gmail.com>
2015-07-01 16:08:36 +0300

    improve students list

--- app/scripts/teacher/teacher.students.list.js
+++ app/scripts/teacher/teacher.students.list.js
@@ -5,27 +5,12 @@
     .module('educationSystemApp.teacher')
     .controller('studentsListCtrl', studentsListCtrl);
   
-  function studentsListCtrl($stateParams, teacherService) {
+  function studentsListCtrl($stateParams, $state, courseAssignments, teacherService) {
     var vm = this;
+    vm.courseAssignments = courseAssignments;
+
     activate();
-    
-
-    teacherService.getCAsForCourse($stateParams.courseId)
-      .then(function(response) {
-        console.log($stateParams);
-        console.log(vm.currentCourseId);
-        vm.courseAssignments = response;
-      });
-
-
-    
     function activate() {
-      if(!$stateParams.courseId) {
-        vm.currentCourseId = 3;
-      }
-      else {
-        vm.currentCourseId = $stateParams.courseId;
-      }
     };
   };
 })();
