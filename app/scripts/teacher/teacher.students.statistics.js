4e91632960010ace92e694bb1b370119b6eea44f
Margarita Vasileva <m.h.vasileva@gmail.com>
2015-07-01 11:24:20 +0300

    add presence table to student-statistics view

--- app/scripts/teacher/teacher.students.statistics.js
+++ app/scripts/teacher/teacher.students.statistics.js
@@ -5,13 +5,24 @@
     .module('educationSystemApp.teacher')
     .controller('studentStatisticsCtrl', studentStatisticsCtrl);
   
-  function studentStatisticsCtrl($stateParams) {
+  function studentStatisticsCtrl($stateParams, tableData, studentService) {
     var vm = this;
     console.log($stateParams);
     activate();
+    var emptyTable = tableData.data;
+    vm.weekdays = tableData.weekdays;
     
     function activate() {
+      vm.courseId = $stateParams.courseId;
+      vm.studentId = $stateParams.studentId;
       
+      
+      studentService.getStudentCheckins(vm.studentId, vm.courseId)
+        .then(function(checkins) {
+          vm.presence = studentService.getPresenceTable(emptyTable, checkins);
+          vm.presencePercentage = studentService.getPresencePercentage(vm.presence);
+        });
+
     };
   };
 })();
