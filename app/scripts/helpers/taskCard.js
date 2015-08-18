0664e3e7a847f94555e99ddee9a73264f297dae9
Margarita Vasileva <m.h.vasileva@gmail.com>
2015-08-18 19:21:15 +0300

    gdd taskCard directive

---
+++ app/scripts/helpers/taskCard.js
@@ -0,0 +1,36 @@
+(function() {
+  'use strict';
+  
+  angular
+    .module('educationSystemApp.helpers')
+    .directive('taskCard', taskCard);
+
+  function taskCard(studentService, $stateParams) {
+    return {
+      restrict: 'EA',
+      require: '^weekCard',
+      templateUrl: 'views/helpers/taskCard.html',
+      scope: {
+        task: '='
+      },
+      link: function(scope, element, attrs, ctrl) {
+        scope.solution = ctrl.getSolution(scope.task.id);
+        scope.submit = function() {
+          if(typeof scope.solution.task === 'undefined') {
+            scope.solution.task = scope.task.id;
+            studentService.submitSolution(scope.solution)
+              .then(function(response) {
+                scope.solution = response;
+              });
+          }
+          else {
+            studentService.updateSolution(scope.task)
+              .then(function(response) {
+                scope.solution = response;
+              });
+          }
+        };
+      }
+    };
+  }
+})();
