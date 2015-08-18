20c1c2704b9dae3f145e27a5c589dcc0b0ed33c3
Margarita Vasileva <m.h.vasileva@gmail.com>
2015-08-18 19:24:20 +0300

    add logic for tasks view

--- app/scripts/student/student.tasks.js
+++ app/scripts/student/student.tasks.js
@@ -5,13 +5,21 @@
     .module('educationSystemApp.student')
     .controller('tasksCtrl', tasksCtrl);
   
-  function tasksCtrl() {
+  function tasksCtrl(studentService, $stateParams) {
     var vm = this;
     
     activate();
     
     function activate() {
-      
+      studentService.getTasks($stateParams.courseId)
+        .then(function(response) {
+          vm.tasks = response;
+        });
+
+      studentService.getSolutions($stateParams.courseId)
+        .then(function(response) {
+          vm.solutions = response;
+        });
     };
   };
 })();
