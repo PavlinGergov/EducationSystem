00f54db60d2cfa5d9717e58b66510eb308eb5234
Margarita Vasileva <m.h.vasileva@gmail.com>
2015-09-09 16:13:21 +0300

    add applicationService

---
+++ app/scripts/application/application.service.js
@@ -0,0 +1,97 @@
+(function() {
+  'use strict';
+  
+  angular
+    .module('educationSystemApp.application')
+    .factory('applicationService', applicationService);
+
+  function applicationService($http, ENV) {
+
+    // TODO:
+    // rename functions
+
+    return {
+      getBundle        : getBundle,
+      getApplication   : getApplication,
+      postApplication  : postApplication,
+      patchApplication : patchApplication,
+      getTask          : getTask,
+      createSolution   : createSolution,
+      getSolution      : getSolution,
+      updateSolution   : updateSolution
+      
+    };
+
+    function getBundle() {
+      return $http.get(ENV.application + 'bundle')
+        .then(function(response) {
+          return response.data;
+        }, function(error) {
+          // handle error
+        });
+    };
+
+    function getApplication() {
+      // return $http.get(ENV.application + 'application/')
+      //   .then(function(response) {
+      //     return response.data;
+      //   }, function(error) {
+      //     // handle error
+      //   });
+    };
+
+    function postApplication(data) {
+      // return $http.post(ENV.application + 'application/', data)
+      //   .then(function(response) {
+      //     return response.data;
+      //   }, function(error) {
+      //     // handle error
+      //   });
+    };
+
+    function patchApplication(data) {
+      // return $http.get(ENV.application + 'application/', data)
+      //   .then(function(response) {
+      //     return response.data;
+      //   }, function(error) {
+      //     // handle error
+      //   });
+    };
+
+    function getTask() {
+      // return $http.get(ENV.application + 'task/')
+      //   .then(function(response) {
+      //     return response.data;
+      //   }, function(error) {
+      //     // handle error
+      //   });
+    };
+
+    function createSolution(data) {
+      // return $http.post(ENV.application + 'task/', data)
+      //   .then(function(response) {
+      //     return response.data;
+      //   }, function(error) {
+      //     // handle error
+      //   });
+    };
+
+    function getSolution() {
+      // return $http.get(ENV.application + 'solution/')
+      //   .then(function(response) {
+      //     return response.data;
+      //   }, function(error) {
+      //     // handle error
+      //   });
+    };
+
+    function updateSolution(data) {
+      // return $http.patch(ENV.application + 'solution/', data)
+      //   .then(function(response) {
+      //     return response.data;
+      //   }, function(error) {
+      //     // handle error
+      //   });
+    };
+  }
+})();
