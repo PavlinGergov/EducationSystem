5158497c02bb169b033ebdc5dceee77365f4bbb0
Margarita Vasileva <m.h.vasileva@gmail.com>
2015-09-16 12:25:08 +0300

    add directive for update profile data

--- app/scripts/application/directives/application-info.js
+++ app/scripts/application/directives/application-info.js
@@ -5,7 +5,7 @@
     .module('educationSystemApp.application')
     .directive('applicationInfo', applicationInfo);
 
-  function applicationInfo() {
+  function applicationInfo(applicationService) {
     return {
       restrict: 'E',
       replace: true,
@@ -14,19 +14,50 @@
         user: '=data'
       },
       link: function(scope, elm, attrs) {
-        scope.$watch('user.skype', function(newValue) {
-          if(newValue || newValue !== scope.user.skype) {
+        scope.$watch('user.skype', function(newValue, oldValue) {
+          if(oldValue) {
             ready('#skype');
           }
           else {
             notReady('#skype');
           }
+          
+          if(newValue && newValue !== oldValue) {
+            applicationService.changeSkype(newValue)
+              .then(function(response) {
+                scope.user.skype = response.skype;
+              });
+            ready('#skype');
+          }
+          else if(newValue === '') {
+            applicationService.changeSkype(newValue)
+              .then(function(response) {
+                scope.user.skype = response.skype;
+              });
+            notReady('#skype');
+          }
         });
-        scope.$watch('user.phone', function(newValue) {
-          if(newValue || newValue !== scope.user.phone) {
+        
+        scope.$watch('user.phone', function(newValue, oldValue) {
+          if(oldValue) {
             ready('#phone');
           }
           else {
+            notReady('#phone');
+          }
+          
+          if(newValue && newValue !== oldValue) {
+            applicationService.changePhone(newValue)
+              .then(function(response) {
+                scope.user.phone = response.phone;
+              });
+            ready('#phone');
+          }
+          else if(newValue === '') {
+            applicationService.changePhone(newValue)
+              .then(function(response) {
+                scope.user.phone = response.phone;
+              });
             notReady('#phone');
           }
         });
