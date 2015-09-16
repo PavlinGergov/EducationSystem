d3daac820ff61869be350be24d425f67cd1131ca
Margarita Vasileva <m.h.vasileva@gmail.com>
2015-09-16 12:22:39 +0300

    add tasks to application

--- app/scripts/application/application.ctrl.js
+++ app/scripts/application/application.ctrl.js
@@ -5,13 +5,14 @@
     .module('educationSystemApp.application')
     .controller('applicationCtrl', applicationCtrl);
 
-  function applicationCtrl(user) {
+  function applicationCtrl(user, tasks) {
     var vm = this;
 
     activate();
 
     function activate() {
       vm.user = user;
+      vm.tasks = tasks;
     };
   }
 })();
